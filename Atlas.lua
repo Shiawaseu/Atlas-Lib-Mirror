f5ca89311c68f51ce6078b5c6efcd114f879b8ad
MEMEZNUT999 <73775954+MEMEZNUT999@users.noreply.github.com>
2023-01-07 16:33:44 +0300

    Update Atlas.lua

--- Atlas.lua
+++ Atlas.lua
@@ -2069,6 +2069,7 @@ do
                     savedKey = nil
                 end
             end
+            wait(2)
             flags = readfile(config)=="" and {} or game:GetService("HttpService"):JSONDecode(readfile(config))
             for i,v in pairs(flags) do
                 if type(v)=="string" then
